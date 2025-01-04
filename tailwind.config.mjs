6f54237297187f6032064f3c27545df3f847556b
Zain Jaafar <zainjaafar120@gmail.com>
2025-01-04 15:21:33 +1300

    added tailwindcss

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    },
+	// plugins: [require("tailwindcss-animate")],
+}
